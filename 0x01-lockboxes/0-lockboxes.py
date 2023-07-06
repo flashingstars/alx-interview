e13a3f572cde9fdfc0472feb6cc0d3c1c5e7a68f
flashingstars <annkarye@gmail.com>
2023-07-06 12:08:19 +0300

    Fixing pep8 style requirements

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-""" Function that returns a boolean answer when all boxes are locked or unlocked """
+"""
+Function that returns a boolean answer when all boxes are locked or unlocked
+"""
 
 
 def canUnlockAll(boxes):
-    """ Function to unlock boxes """
+    """
+    Function to determine if all boxes can be opened.
+
+    Args: 
+        boxes (list of lists): List of boxes, where each box may contain keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes) # Finding total box number
     unlocked = [False] * n
     unlocked[0] = True # First box is unlocked
 
     keys = boxes[0]
 
-    """ Iterating through the available keys """
+    # Iterating through the available keys
     while keys:
         key = keys.pop()
 
         if key < n and not unlocked[key]:
-            """ If key corresponds to box, unlock and add keys to unlocked"""
+            # If key corresponds to box, unlock and add keys to unlocked
             unlocked[key] = True
             keys.extend(boxes[key])
 
