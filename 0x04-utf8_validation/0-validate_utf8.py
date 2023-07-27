2e34d8ff4eb7e3fd9598a669ea79cd378911f93a
flashingstars <annkarye@gmail.com>
2023-07-27 15:30:43 +0300

    UTF_8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Function that returns a boolean answer when data represents a valid utf8 character
+"""
+
+
+def validUTF8(data):
+    """
+    Function that determines whether the given data is utf8.
+
+    Args:
+        data (data): Multiple characters represented by a
+        list of integers.
+    Returns:
+        bool: True if data is a valid UTF-8, False otherwise.
+    """
+    counter = 0
+
+    #Iterate through each integer in the list
+    for num in data:
+        #If counter is 0, check the number of leading 1's present
+        if counter == 0:
+            if (num >> 5) == 0b110:
+                counter = 1
+            elif (num >> 4) == 0b1110:
+                counter = 2
+            elif (num >> 3) == 0b11110:
+                counter = 3
+            elif (num >> 7) != 0:
+                return False
+            #If counter is not 0, check if the current integer is a continuation byte
+        else:
+            if (num >> 6) != 0b10:
+                return False
+            
+    #A valid counter is 0 after iterating through all the integers
+    return counter == 0
+ 
